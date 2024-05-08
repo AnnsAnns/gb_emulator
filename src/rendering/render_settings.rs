016a4f483fce44f17f0bfc18d1fcb2aeb0e9071f
Michael Vogt <michaelvogt02@live.de>
2024-05-08 19:42:01 +0200

    implement Default Values for GbSettings

--- src/rendering/render_settings.rs
+++ src/rendering/render_settings.rs
@@ -4,3 +4,17 @@ pub struct GbSettings {
     pub scaling: f32,
     pub palette: [Color; 4],
 }
+
+impl Default for GbSettings {
+    fn default() -> Self {
+        GbSettings {
+            scaling: 4.0,
+            palette: [
+                Color::new(1.00, 1.00, 1.00, 0.00),
+                Color::new(0.06, 0.35, 0.06, 1.00),
+                Color::new(0.12, 0.54, 0.12, 1.00),
+                Color::new(0.18, 0.83, 0.18, 1.00)
+            ]
+        }
+    }
+}
