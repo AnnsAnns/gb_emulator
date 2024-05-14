a57bb5987cb4cbaff9301acdf556a29a1ac8bb71
Michael Vogt <michaelvogt02@live.de>
2024-05-14 11:05:45 +0200

    Add WIP VRAM to Background image function

--- src/rendering/tiles.rs
+++ src/rendering/tiles.rs
@@ -1,8 +1,7 @@
 use std::usize;
 
 use macroquad::prelude::*;
-
-use crate::{memory::Memory, GbSettings};
+use crate::memory::Memory;
 
 pub fn update_tile_atlas(
     tile_index: u32,
@@ -45,5 +44,32 @@ pub fn update_atlas_from_memory(
         }
 
         update_tile_atlas(tile_index as u32, &tile_data, tile_atlas, palette);
+    }
+}
+
+pub fn update_background_from_memory(memory: &Memory, tile_atlas: &Image, background: &mut Image) {
+    // Tile Map Base Address
+    let base_addr: u16 = 0x9800;
+
+    for bg_index in 0..(32 * 32) {
+        let tile_index = memory.read_byte(base_addr + bg_index) as u32 + 256; //TODO USE IO Registers
+        //println!("BG-Idx: {:#X} -> {:#X}", bg_index, tile_index);
+        let tile = tile_atlas.sub_image(Rect {
+            x: ((tile_index % 16) * 8) as f32,
+            y: ((tile_index / 16) * 8) as f32,
+            w: 8.,
+            h: 8.,
+        });
+
+        let bg_offset_x = (bg_index as u32%32)*8;
+        let bg_offset_y = (bg_index as u32/32)*8;
+
+        for pixel_index in 0..(8 * 8) {
+            background.set_pixel(
+                pixel_index % 8 + bg_offset_x,
+                pixel_index / 8 + bg_offset_y,
+                tile.get_pixel(pixel_index % 8, pixel_index / 8),
+            );
+        }
     }
 }
