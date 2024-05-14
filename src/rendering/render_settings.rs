80e45a833deff696e30e82691c70210c13b7ea75
Michael Vogt <michaelvogt02@live.de>
2024-05-14 11:03:29 +0200

    Fix Default Color Palette

--- src/rendering/render_settings.rs
+++ src/rendering/render_settings.rs
@@ -10,10 +10,10 @@ impl Default for GbSettings {
         GbSettings {
             scaling: 4.0,
             palette: [
-                Color::new(1.00, 1.00, 1.00, 0.00),
-                Color::new(0.06, 0.35, 0.06, 1.00),
+                Color::new(1.00, 1.00, 1.00, 1.00),
+                Color::new(0.18, 0.83, 0.18, 1.00),
                 Color::new(0.12, 0.54, 0.12, 1.00),
-                Color::new(0.18, 0.83, 0.18, 1.00)
+                Color::new(0.06, 0.15, 0.06, 1.00)
             ]
         }
     }
