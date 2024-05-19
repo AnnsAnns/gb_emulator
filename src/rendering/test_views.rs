c289a29850d121c30574dbe2b47f3d82742a901a
Michael Vogt <michaelvogt02@live.de>
2024-05-19 15:53:12 +0200

    Bind drawing functions to structs

--- src/rendering/test_views.rs
+++ src/rendering/test_views.rs
@@ -1,86 +1,40 @@
-use macroquad::prelude::*;
 use crate::cpu;
 use crate::memory::raw_memory_operations::test_helper::*;
 use crate::rendering::tiles::*;
 use crate::rendering::views::*;
-
+use macroquad::prelude::*;
 
 #[macroquad::test]
-async fn golden_image_vram_viewer() {
+async fn golden_image_tile_viewer() {
     // Inititalize General Settings
     const PALETTE: [Color; 4] = [
         Color::new(1.00, 1.00, 1.00, 1.00),
         Color::new(0.18, 0.83, 0.18, 1.00),
         Color::new(0.12, 0.54, 0.12, 1.00),
-        Color::new(0.06, 0.15, 0.06, 1.00)
+        Color::new(0.06, 0.15, 0.06, 1.00),
     ];
     const SCALING: f32 = 4.0;
 
-    let mut tile_atlas = Image::gen_image_color(8 * 16, 8 * 24, WHITE);
-
-    request_new_screen_size(
-        8.0 * 16.0 * SCALING,
-        8.0 * 24.0 * SCALING,
-    );
+    let mut atlas = Image::gen_image_color(8 * 16, 8 * 24, WHITE);
 
     let mut cpu = cpu::CPU::new();
 
-    file_to_memory(
-        &mut cpu.get_memory(),
-        0x8000,
-        "test_files/cgbBCE1-VRAM.bin",
-    );
+    file_to_memory(&mut cpu.get_memory(), 0x8000, "test_files/cgbBCE1-VRAM.bin");
 
-    update_atlas_from_memory(
-        &cpu.get_memory(),
-        16 * 24,
-        &mut tile_atlas,
-        &PALETTE,
-    );
+    let mut tile_viewer = TileViewer {
+        offset_x: 0.0,
+        offset_y: 0.0,
+        scaling: SCALING,
+    };
 
-    loop {
-        draw_tile_viewer(0.0, 0.0, &tile_atlas, SCALING);
+    let window_size = tile_viewer.size();
 
-        next_frame().await;
-    }
-}
+    request_new_screen_size(window_size.x, window_size.y);
 
-#[macroquad::test]
-async fn golden_image_layout() {
-    // Inititalize General Settings
-    const PALETTE: [Color; 4] = [
-        Color::new(1.00, 1.00, 1.00, 1.00),
-        Color::new(0.18, 0.83, 0.18, 1.00),
-        Color::new(0.12, 0.54, 0.12, 1.00),
-        Color::new(0.06, 0.15, 0.06, 1.00)
-    ];
-    const SCALING: f32 = 4.0;
-
-
-    let mut tile_atlas = Image::gen_image_color(8 * 16, 8 * 24, WHITE);
-    let combined_image = Image::gen_image_color(160, 144, GREEN);
-
-    #[rustfmt::skip]
-    let test_tile: [u8; 16] = [
-        0xFF, 0x00, 0x7E, 0xFF, 0x85, 0x81, 0x89, 0x83, 
-        0x93, 0x85, 0xA5, 0x8B, 0xC9, 0x97, 0x7E, 0xFF
-    ];
-
-    request_new_screen_size(
-        (160.0 + 8.0 * 16.0) * SCALING + 15.0,
-        (8.0 * 24.0) * SCALING + 25.0,
-    );
+    update_atlas_from_memory(&cpu.get_memory(), 16 * 24, &mut atlas, &PALETTE);
 
     loop {
-        update_tile_atlas(9, &test_tile, &mut tile_atlas, &PALETTE);
-
-        draw_gb_display(5.0, 5.0, &combined_image, SCALING);
-        draw_tile_viewer(
-            combined_image.width() as f32 * SCALING + 10.0,
-            5.0,
-            &tile_atlas,
-            SCALING,
-        );
+        tile_viewer.draw(&atlas);
 
         next_frame().await;
     }
@@ -93,7 +47,7 @@ async fn golden_image_background_viewer() {
         Color::new(1.00, 1.00, 1.00, 1.00),
         Color::new(0.18, 0.83, 0.18, 1.00),
         Color::new(0.12, 0.54, 0.12, 1.00),
-        Color::new(0.06, 0.15, 0.06, 1.00)
+        Color::new(0.06, 0.15, 0.06, 1.00),
     ];
     const SCALING: f32 = 4.0;
 
@@ -102,31 +56,81 @@ async fn golden_image_background_viewer() {
 
     let mut cpu = cpu::CPU::new();
 
-    file_to_memory(
-        &mut cpu.get_memory(),
-        0x8000,
-        "test_files/Mindy1-VRAM.bin",
-    );
+    let mut background_viewer = BackgroundViewer {
+        offset_x: 0.0,
+        offset_y: 0.0,
+        scaling: SCALING,
+    };
 
-    request_new_screen_size(
-        (32.0 * 8.0 + 16.0 * 8.0) * SCALING,
-        (32.0 * 8.0) * SCALING,
-    );
+    file_to_memory(&mut cpu.get_memory(), 0x8000, "test_files/Mindy1-VRAM.bin");
+
+    let window_size = background_viewer.size();
+
+    request_new_screen_size(window_size.x, window_size.y);
 
     loop {
-
-        update_atlas_from_memory(&cpu.get_memory(), 16*24, &mut tile_atlas, &PALETTE);
-
+        update_atlas_from_memory(&cpu.get_memory(), 16 * 24, &mut tile_atlas, &PALETTE);
         update_background_from_memory(&cpu.get_memory(), &tile_atlas, &mut background_image);
+        background_viewer.draw(&background_image);
+        next_frame().await;
+    }
+}
 
-        draw_tile_viewer(
-            (32.0 * 8.0) * SCALING + 10.0,
-            0.0,
-            &tile_atlas,
-            SCALING,
-        );
+#[macroquad::test]
+async fn golden_image_layout() {
+    // Inititalize General Settings
+    const PALETTE: [Color; 4] = [
+        Color::new(1.00, 1.00, 1.00, 1.00),
+        Color::new(0.18, 0.83, 0.18, 1.00),
+        Color::new(0.12, 0.54, 0.12, 1.00),
+        Color::new(0.06, 0.15, 0.06, 1.00),
+    ];
+    const SCALING: f32 = 4.0;
 
-        draw_background_viewer(0.0, 0.0, &background_image, SCALING);
+    let final_image = Image::gen_image_color(160, 144, GREEN);
+    let mut gb_display = GbDisplay {
+        offset_x: 5.0,
+        offset_y: 5.0,
+        scaling: SCALING,
+    };
+    let gb_display_size = gb_display.size(&final_image);
+
+    let mut background_viewer = BackgroundViewer {
+        offset_x: gb_display_size.x + 10.0,
+        offset_y: 5.0,
+        scaling: SCALING / 2.0,
+    };
+    let mut background_image = Image::gen_image_color(32 * 8, 32 * 8, PINK);
+    let background_viewer_size = background_viewer.size();
+
+    let mut tile_atlas = Image::gen_image_color(8 * 16, 8 * 24, WHITE);
+    let mut tile_viewer = TileViewer {
+        offset_x: gb_display_size.x + background_viewer_size.x + 15.0,
+        offset_y: 5.0,
+        scaling: SCALING,
+    };
+    let tile_viewer_size = tile_viewer.size();
+
+    request_new_screen_size(
+        background_viewer_size.x + tile_viewer_size.x + gb_display_size.x + 20.0,
+        tile_viewer_size.y + 10.0,
+    );
+
+    let mut cpu = cpu::CPU::new();
+
+    file_to_memory(&mut cpu.get_memory(), 0x8000, "test_files/Mindy1-VRAM.bin");
+
+    update_atlas_from_memory(&cpu.get_memory(), 16 * 24, &mut tile_atlas, &PALETTE);
+    update_background_from_memory(&cpu.get_memory(), &tile_atlas, &mut background_image);
+
+    loop {
+        //update_tile_atlas(9, &test_tile, &mut tile_atlas, &PALETTE);
+
+        background_viewer.draw(&background_image);
+
+        gb_display.draw(&final_image);
+
+        tile_viewer.draw(&tile_atlas);
 
         next_frame().await;
     }
